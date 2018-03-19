28a4c4e03a704814783787cefd20b6f179632f65
Mindaugas Šimulionis <Mindaugas.Simulionis@bite.lt>
2018-03-19 19:36:41 +0200

    Failo sukūrimas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+
 public class Main {
 
     public static void main(String[] args) {
 
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("login.txt"))
+        ) {
+            bufferedWriter.write("admin, admin");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
